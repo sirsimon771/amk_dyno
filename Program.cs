58cec96be1fcff58709de261061185daa10eefb6
sirsimon771 <simon.dischl@yahoo.de>
2020-09-08 16:50:58 +0200

    edited code

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace amk_dyno
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
