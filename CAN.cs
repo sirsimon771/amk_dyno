09bbb20a34c48edb05fa1f9ba6210054c40aae90
sirsimon771 <simon.dischl@yahoo.de>
2020-09-11 02:27:41 +0200

    added Algorithm and CAN classes back

---
+++ CAN.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace amk_dyno
+{
+    /// <summary>
+    /// contains the read and write functions to communicate with the CAN adapter
+    /// using the provided C# API of the driver
+    /// just copy from example and open a cmd for setup
+    /// </summary>
+    class CAN
+    {
+        public void CanSetup()
+        {
+
+        }
+
+        public bool CanSend()
+        {
+            return true;
+        }
+
+        public double CanReceive()
+        {
+            return 1;
+        }
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,15 +20,36 @@ namespace amk_dyno
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool toggle = false;
+
         public MainWindow()
         {
             InitializeComponent();
-            button1.Content = "test12345";
-            button1.Height = 50;
-            button1.Width = 600;
-            Canvas.SetLeft(button1, canvas1.ActualWidth / 2 - button1.ActualWidth / 2);
-            Canvas.SetTop(button1, canvas1.ActualHeight / 2 - button1.ActualHeight / 2);
-            button1.Content = String.Format("cw: {0} ch: {1} bw: {2} bh: {3}", canvas1.ActualWidth, canvas1.ActualHeight, button1.ActualWidth, button1.ActualHeight);
+            startstop.Content = "test12345";
+            startstop.Height = 50;
+            startstop.Width = 600;
+            startstop.ToolTip = "start-stop button";
+            Canvas.SetLeft(startstop, canvas1.ActualWidth / 2 - startstop.ActualWidth / 2);
+            Canvas.SetTop(startstop, canvas1.ActualHeight / 2 - startstop.ActualHeight / 2);
+            startstop.Content = String.Format("cw: {0} ch: {1} bw: {2} bh: {3}", canvas1.ActualWidth, canvas1.ActualHeight, startstop.ActualWidth, startstop.ActualHeight);
+        }
+
+        private void StartStop(object sender, RoutedEventArgs e)
+        {
+            //start stop button press
+            toggle = !toggle;
+
+            if (toggle)
+            {
+                //turn on
+                startstop.Background = new SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                //turn off
+                startstop.Background = new SolidColorBrush(Colors.Red);
+            }
+
         }
     }
 }
